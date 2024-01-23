1879fa7fc84aa50727d0bc5d1f7ba362824f2fe6
Moni Akhtar <moni.akhtar1066@gmail.com>
2024-01-23 20:13:16 +0600

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Search from './pages/Search';
+import Projects from './pages/Projects';
+import Dashboard from './pages/Dashboard';
+import CreatePost from './pages/CreatePost';
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/sign-in' element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/projects' element={<Projects />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+        <Route path='/create-post' element={<CreatePost />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
