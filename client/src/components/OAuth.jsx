960ee2bef50c528ac865a10e76bb8a3275a1d535
Moni Akhtar <moni.akhtar1066@gmail.com>
2024-01-30 10:35:13 +0600

    connect to the firebase

--- client/src/components/OAuth.jsx
+++ client/src/components/OAuth.jsx
@@ -4,11 +4,16 @@ import { AiFillGoogleCircle } from "react-icons/ai";
 
 
 export default function OAuth() {
+   const handleGoogleClick = () => {
+      
+   }
+   
    return (
       <Button
          type="button"
          gradientDuoTone="pinkToOrange"
          outline
+         onClick={handleGoogleClick}
       >
          <AiFillGoogleCircle className="w-6 h-6 mr-2" />
          Continue with Google
---
+++ client/src/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.VITE_FIREBASE_API_KEY,
+  authDomain: "jotjive.firebaseapp.com",
+  projectId: "jotjive",
+  storageBucket: "jotjive.appspot.com",
+  messagingSenderId: "720031766428",
+  appId: "1:720031766428:web:8986f4beab8f711dc4da59",
+  measurementId: "G-CWWNTTN469"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
