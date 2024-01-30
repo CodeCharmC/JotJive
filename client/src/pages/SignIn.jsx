607a2b9f558db3359a2cd930796fdc74025b31be
Moni Akhtar <moni.akhtar1066@gmail.com>
2024-01-30 10:12:55 +0600

    add Continue with Google button

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -3,6 +3,7 @@ import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { signInStart, signInSuccess, signInFailure } from "../redux/user/userSlice";
 import { useDispatch, useSelector } from "react-redux";
+import OAuth from "../components/OAuth";
 
 export default function SignIn() {
    const [formData, setFormData] = useState({});
@@ -102,7 +103,8 @@ export default function SignIn() {
                               Sign In
                            </span> 
                      )}                     
-                  </Button>                 
+                  </Button> 
+                  <OAuth />
                </form>
                <div className="flex gap-2 text-sm mt-5">
                   <span>Dont have an account?</span>
