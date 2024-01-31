bae2554aca5d1d77d827e9742c99305479b7ad0b
Moni Akhtar <moni.akhtar1066@gmail.com>
2024-01-31 23:34:14 +0600

    update header component with user data

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,12 @@
-import { Button, Navbar, TextInput } from 'flowbite-react';
+import { Avatar, Button, Dropdown, Navbar, TextInput } from 'flowbite-react';
 import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
 import { FaMoon } from 'react-icons/fa';
+import {useSelector} from 'react-redux';
 
 
 export default function Header() {
+   const currentUser = useSelector((state) => state.user.currentUser);
    const path = useLocation().pathname;
    const location = useLocation();
 
@@ -46,9 +48,34 @@ export default function Header() {
             <Link
                to='/sign-in'
             >
-               <Button gradientDuoTone='purpleToBlue' outline>
-                  Sign In
-               </Button>
+               {currentUser ? (
+                  <Dropdown
+                     arrowIcon={false}
+                     inline
+                     label={
+                        <Avatar
+                           alt='User'
+                           img={currentUser?.profilePicture}
+                           rounded
+                        />
+                     }
+                  >
+                     <Dropdown.Header>
+                        <span className='block text-sm'>@{currentUser.username}</span>
+                        <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
+                     </Dropdown.Header>
+                     <Link to='/dashboard?tab=profile'>
+                        <Dropdown.Item>Profile</Dropdown.Item>
+                     </Link>
+                     <Dropdown.Divider />
+                     <Dropdown.Item>Sign out</Dropdown.Item>
+                  </Dropdown>
+               ):(
+                  <Button gradientDuoTone='purpleToBlue' outline>
+                     Sign In
+                  </Button>)
+               }
+                      
             </Link>
             <Navbar.Toggle />            
          </div>
