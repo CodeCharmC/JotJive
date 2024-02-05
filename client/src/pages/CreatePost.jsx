5d77e39de6924107c5888da4baa27f844c6d4870
Moni Akhtar <moni.akhtar1066@gmail.com>
2024-02-05 19:15:35 +0600

    complate image upload functionality

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,8 +1,54 @@
 import { Alert, Button, FileInput, Select, TextInput } from 'flowbite-react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import {app} from '../firebase';
+import { useState } from 'react';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
 
 export default function CreatePost() {
+   const [file, setFile] = useState(null);
+   const [imageUploadProgress, setImageUploadProgress] = useState(null);
+   const [imageUploadError, setImageUploadError] = useState(null);
+   const [formData, setFormData] = useState({ })
+
+
+   const handleUpdloadImage = async () => {
+      try {
+         if (!file) {
+            setImageUploadError('No file selected');
+            return
+         }
+         setImageUploadError(null);
+         const storage = getStorage(app);
+         const fileName = new Date().getTime() + file.name;
+         const storageRef = ref(storage, fileName);
+         const uploadTask = uploadBytesResumable(storageRef, file);
+         uploadTask.on(
+            'state_changed',
+            (snapshot) => {
+               const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+               setImageUploadProgress(progress.toFixed(0));
+            },
+            (error) => {
+               setImageUploadProgress(null);
+               setImageUploadError('Image upload failed');
+            },
+            () => {
+               getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                  setImageUploadProgress(null);
+                  setImageUploadError(null);
+                  setFormData({ ...formData, image: downloadURL });                  
+               });
+            }
+         )
+      } catch (error) {
+         setImageUploadError(error.message);
+      }   
+   }
+
    return (
       <div className='p-3 max-w-3xl mx-auto min-h-screen'>
          <h1 className='text-center text-3xl my-7 font-semibold'>Create a post</h1>
@@ -28,22 +74,47 @@ export default function CreatePost() {
                <FileInput
                   type='file'
                   accept='image/*'
+                  onChange={(e) => setFile(e.target.files[0])}
                />
                <Button
                   type='button'
                   gradientDuoTone='purpleToBlue'
                   size='sm'
                   outline
+                  onClick={handleUpdloadImage}
                >
-                  upload image
+                  {imageUploadProgress ? (
+                     <div className='w-16 h-16'>
+                        <CircularProgressbar
+                           value={imageUploadProgress}
+                           text={`${imageUploadProgress || 0}%`}
+                        />
+                     </div>
+                  ) : (
+                     'Upload Image'
+                  )}
                </Button>
-               <ReactQuill
-                  theme='snow'
-                  placeholder='Write something...'
-                  className='h-72 mb-12'
-                  required
+            </div>
+            {imageUploadError && (
+               <Alert
+                  color='failure'
+               >
+                  {imageUploadError}
+               </Alert>
+            )}
+            {formData.image && (
+               <img
+                  src={formData.image}
+                  alt='cover image'
+                  className='w-full h-72 object-cover'
                />
-            </div>          
+            )}
+            <ReactQuill
+               theme='snow'
+               placeholder='Write something...'
+               className='h-72 mb-12'
+               required
+            />                      
             <Button type='submit' gradientDuoTone='purpleToPink'>
                Publish
             </Button>
