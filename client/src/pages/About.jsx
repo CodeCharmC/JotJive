565b2d18a3cabe426e29d73e5f869a9cc2b1c5c9
Moni Akhtar <moni.akhtar1066@gmail.com>
2024-02-10 18:40:22 +0600

    complate about apge

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,24 @@
-import React from 'react'
-
 export default function About() {
    return (
-      <div>About</div>
+      <div className='min-h-screen flex items-center justify-center'>
+         <div className='max-w-2xl mx-auto p-3 text-center'>
+            <div>
+               <h1 className='text-3xl font font-semibold text-center my-7'>
+                  About JotJive
+               </h1>
+               <div className='text-md text-gray-500 flex flex-col gap-6'>
+                  <p>
+                     Welcome to JotJive! This vibrant space was created by Moni Akhtar, a passionate wed developer and writer dedicated to the art of storytelling. JotJive is my personal practice project for web development. I would like to thank Sir Sahand Ghavidel for his guidance and inspiration. With  JotJive, I have learned a lot
+                  </p>
+                  <p>
+                     Here, you'll discover a treasure trove of articles, stories, and insights covering a wide range of topics, from technology to literature, from travel to personal growth. I will constantly explore new horizons and craft fresh content to spark your curiosity and inspire your creativity.
+                  </p>
+                  <p>
+                     Stay connected with JotJive and embark on a journey of discovery, expression, and connection. Together, let's celebrate the beauty of storytelling!
+                  </p>
+               </div>
+            </div>
+         </div>
+      </div>
    );
-}
+} 
